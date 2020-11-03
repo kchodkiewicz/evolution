86cb05f170dd23c5596b44ca2e8d1b9167c55329
kchodkiewicz <32763541+kchodkiewicz@users.noreply.github.com>
2020-11-03 22:19:16 +0100

    feat: synced with java project

--- project/Phenotype.py
+++ project/Phenotype.py
@@ -6,10 +6,11 @@ class Phenotype:
     def __init__(self, committee, gen_length):
         self.committee = committee
         self.genLength = gen_length
-        self.genes = [False for x in range(self.genLength)]
         self.fitness = 0.0
+        self.normalizedFitness = 0.0
         self.isBest = False
         self.isClassificationFinished = False
+        self.genes = [False for x in range(self.genLength)]
         self.create_random_genes()
 
     # generate 10 positive genes (classifiers)
@@ -21,14 +22,50 @@ class Phenotype:
                 self.genes[index] = True
                 it += 1
 
-    def load_genes(self, genes):
-        self.genes = genes
+    def copy(self, parent):
+        # TODO fix dat shit
+        self.committee = parent.committee
+        self.genLength = parent.genesLength
+        self.genes = parent.get_genes()
+
+    @property
+    def genes(self):
+        return self.genes
+
+    @genes.setter
+    def genes(self, value):
+        if len(value) == self.genLength:
+            self.genes = value
+
+    @property
+    def fitness(self):
+        return self.fitness
+
+    @fitness.setter
+    def fitness(self, value):
+        if value >= 0.0:
+            self.fitness = value
+
+    @property
+    def normalizedFitness(self):
+        return self.normalizedFitness
+
+    @normalizedFitness.setter
+    def normalizedFitness(self, value):
+        if 0.0 <= value <= 1.0:
+            self.normalizedFitness = value
+
+    def classification_did_finish(self):
+        return self.isClassificationFinished
 
     def calc_fitness(self):
         pass
 
     def run(self):
+        self.isClassificationFinished = False
         for gen in self.genes:
             if gen:
                 # choose classifiers from list and execute
                 pass
+        self.calc_fitness()
+        self.isClassificationFinished = True
--- project/Population.py
+++ project/Population.py
@@ -8,6 +8,8 @@ class Population:
     def __init__(self, size, committee, gen_length):
         self.size = size
         self.classifierCommittee = committee
+        self.genLength = gen_length
+        self.genNo = 0
         self.mutation_ratio = 3
         self.phenotypes = [Phenotype(self.classifierCommittee, gen_length) for i in range(self.size)]
         self.bestInGen = None
@@ -23,26 +25,29 @@ class Population:
     # choose genes for child1 and remove them from true_genes
     # get list of genes that duplicate in both parents (AND)
     # add to child2 all duplicated genes and remaining genes from true_genes
-    def cross(self, father, mother):
-        child1 = [False for _ in range(len(father.genes))]
-        child2 = [False for _ in range(len(father.genes))]
+    def cross(self, parent_first, parent_second):
+        child_first = [False for _ in range(len(parent_first.genes))]
+        child_second = [False for _ in range(len(parent_first.genes))]
         true_genes = []
         duplicates = []
-        for i in range(0, len(father.genes)):
-            if father.genes[i] or mother.genes[i]:
+        for i in range(0, len(parent_first.genes)):
+            if parent_first.genes[i] or parent_second.genes[i]:
                 true_genes.append(i)
-            if father.genes[i] and mother.genes[i]:
+            if parent_first.genes[i] and parent_second.genes[i]:
                 duplicates.append(i)
         for _ in range(self.classifierCommittee):
             index = random.randint(0, len(true_genes) - 1)
-            child1[true_genes[index]] = True
+            child_first[true_genes[index]] = True
             true_genes.pop(index)
         for index in true_genes:
-            child2[index] = True
+            child_second[index] = True
         for index in duplicates:
-            child2[index] = True
-        father.load_genes(child1)
-        mother.load_genes(child2)
+            child_second[index] = True
+        child1st = Phenotype(parent_first.committee, parent_first.genLength)
+        child2nd = Phenotype(parent_second.committee, parent_second.genLength)
+        child1st.genes = child_first
+        child2nd.genes = child_second
+        return child1st, child2nd
 
     # Create lists of True and False values in genes
     # Get random amount of mutations (between 0 and 3)
@@ -71,12 +76,53 @@ class Population:
         for index in negative_values:
             phenotype.genes[index] = False
 
+    def find_best_in_gen(self):
+        best = Phenotype(self.classifierCommittee, self.genLength)
+        for phenotype in self.phenotypes:
+            if phenotype.fitness > best.fitness:
+                best = phenotype
+        return best
+
+    def find_parent(self):
+        index = 0
+        rand = random.uniform(0, 1)
+        while rand > 0:
+            rand = rand - self.phenotypes[index].normalizedFitness
+            index += 1
+
+        return self.phenotypes[index - 1]
+
     # Create sorted (based on fitness) list of all phenotypes
     # Generate weighted list with best phenotypes having most slots
     # Cross with randomly chosen parents from weighted list
     # Mutate phenotypes
     def select(self):
-        pass
+        total_fitness = 0.0
+        for phenotype in self.phenotypes:
+            total_fitness += phenotype.fitness
+
+        champ = self.find_best_in_gen()
+
+        for phenotype in self.phenotypes:
+            phenotype.normalizedFitness = phenotype.fitness / total_fitness
+
+        new_generation = [Phenotype(self.classifierCommittee, self.genLength) for i in range(self.size)]
+
+        # TODO multi-threading
+        i = 0
+        while (i + 1) < len(self.phenotypes):
+            first_parent = self.find_parent()
+            second_parent = self.find_parent()
+
+            child1st, child2nd = self.cross(first_parent, second_parent)
+            new_generation[i] = child1st
+            new_generation[i + 1] = child2nd
+            i += 2
+        for phenotype in self.phenotypes:
+            self.mutate(phenotype)
+
+        new_generation[0] = champ
 
     def run(self):
-        pass
+        for phenotype in self.phenotypes:
+            phenotype.run()
